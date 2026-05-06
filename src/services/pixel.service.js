f2fe30d1126bc70012975ded264ca231adf4df8e
CuongND <cuongnd@archergame.mobi>
2026-05-06 16:33:27 +0700

    .

--- src/services/pixel.service.js
+++ src/services/pixel.service.js
@@ -1,6 +1,18 @@
-﻿const { PIXEL_GIF } = require("../utils/gif");
+const { trackingResponseMode } = require("../config");
+const { PIXEL_GIF } = require("../utils/gif");
 
 exports.sendPixel = (res) => {
+    if (String(trackingResponseMode).trim().toLowerCase() !== "pixel") {
+        res
+            .status(200)
+            .set({
+                "Content-Type": "text/plain; charset=utf-8",
+                "Content-Length": "2",
+            })
+            .end("OK");
+        return;
+    }
+
     res
         .status(200)
         .set({
